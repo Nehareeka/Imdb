171ca31c01ac5aaed23d1d62ccc2cb1bc9b8467e
ICEPOR\nrayat <nrayat@ivycomptech.com>
2018-12-04 19:36:36 +0530

    Added client side angular files and integrated with aspnet core server side code

---
+++ IMDB/ClientApp/app/movie/movieList.component.ts
@@ -0,0 +1,22 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    selector: "movie-list",
+    templateUrl: "movieList.component.html",
+    styleUrls: []
+})
+export class MovieListComponent {
+    public movies = [
+        {
+            title: "Avatar",
+            year: "1999"
+        },
+        {
+            title: "Raid",
+            year: "2018"
+        },
+        {
+            title: "Rustom",
+            year: "2016"
+        }];
+}
